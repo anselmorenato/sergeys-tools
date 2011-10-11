06b63ae32d7fa3bc00b27ab406aa0b0304c933b8
sergey.selivanov <sergey.selivanov@41c48116-cc6a-11de-b743-054a4f195298>
2011-10-11 03:40:11 +0000

    ie detect relocated cache

--- WebCacheDigger/trunk/src/org/sergeys/webcachedigger/logic/InternetExplorer.java
+++ WebCacheDigger/trunk/src/org/sergeys/webcachedigger/logic/InternetExplorer.java
@@ -2,6 +2,9 @@ package org.sergeys.webcachedigger.logic;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -57,13 +60,13 @@ public class InternetExplorer extends AbstractBrowser {
 	@Override
 	protected List<File> collectExistingCachePaths() throws Exception {
 		List<File> paths = new ArrayList<File>();
-
-		// see IE settings for real path. This is default path.
-		
+											
+ 		// Try default paths.
 		if(System.getenv("LOCALAPPDATA") != null){
 			// vista/7
 			String path = System.getenv("LOCALAPPDATA") + File.separator +
-					"Microsoft" + File.separator + "Windows" + File.separator + "Temporary Internet Files";	// win7 ie9
+					"Microsoft" + File.separator + "Windows" + File.separator + "Temporary Internet Files" + File.separator + 
+					"Content.IE5";	// win7 ie9
 			
 			File f = new File(path); 
 			if(f.isDirectory()){
@@ -73,15 +76,35 @@ public class InternetExplorer extends AbstractBrowser {
 						
 		}				
 		else if(System.getenv("USERPROFILE") != null){
-			// 2000/xp %USERPROFILE%\Local Settings\Temporary Internet Files
+			// 2000/xp
 			String path = System.getenv("USERPROFILE") + File.separator +
-					"Local Settings" + File.separator + "Temporary Internet Files";	// winxp ie6
+					"Local Settings" + File.separator + "Temporary Internet Files" + File.separator + 
+					"Content.IE5";	// winxp ie6
 			
 			File f = new File(path); 
 			if(f.isDirectory()){
 				paths.add(f);
 				System.out.println("Actual path to search (win): " + path);
 			}						
+		}
+
+		if(paths.size() == 0 && System.getenv("OS") != null && System.getenv("OS").equals("Windows_NT")){
+			// not found at default location, get relocated actual path from registry
+			// http://www.winxptutor.com/movetif.htm
+			// http://stackoverflow.com/questions/62289/read-write-to-windows-registry-using-java
+			// http://www.coderanch.com/t/132336/gc/Read-Windows-Registry-java
+			
+			String path = readRegistry("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\" 
+	                 + "Explorer\\Shell Folders", "Cache", "REG_SZ");
+			System.out.println("Registry path (win): " + path);
+			if(path != null){
+				path = path + File.separator + "Content.IE5";
+				File f = new File(path); 				
+				if(f.isDirectory()){
+					paths.add(f);
+					System.out.println("Actual path to search (win): " + path);
+				}
+			}
 		}
 		
 		return paths;
@@ -130,4 +153,67 @@ public class InternetExplorer extends AbstractBrowser {
 		return "Internet Explorer";
 	}
 	
+	/**
+	 * Works on windows 7, xp
+	 * 
+	 * @param location
+	 * @param key
+	 * @param expectedType in form like "REG_SZ", as reg.exe returns
+	 * @return
+	 */
+    protected static final String readRegistry(String location, String key, String expectedType){
+        try {
+            // Run reg query, then read output with StreamReader (internal class)
+            Process process = Runtime.getRuntime().exec("reg query " + 
+                    '"'+ location + "\" /v " + key);
+
+            StreamReader reader = new StreamReader(process.getInputStream());
+            reader.start();
+            process.waitFor();
+            reader.join();
+            String output = reader.getResult();
+
+            // Output has the following format:
+            // \n<Version information>\n\n<key>\t<registry type>\t<value>
+//            if(!output.contains("\t")){
+//                    return null;
+//            }
+
+            // note no tabs on win7
+            
+            // Parse out the value
+            String[] parsed = output.split(expectedType);
+            return parsed[parsed.length-1].trim();
+        }
+        catch (Exception e) {
+        	e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    static class StreamReader extends Thread {
+        private InputStream is;
+        private StringWriter sw= new StringWriter();
+
+        public StreamReader(InputStream is) {
+            this.is = is;
+        }
+
+        public void run() {
+            try {
+                int c;
+                while ((c = is.read()) != -1)
+                    sw.write(c);
+            }
+            catch (IOException e) {
+            	e.printStackTrace();
+            }
+        }
+
+        public String getResult() {
+            return sw.toString();
+        }
+    }
+
 }
