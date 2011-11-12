0a6d63135b90f992eb02e6ac48c8eb837637bf0f
sergey.selivanov <sergey.selivanov@41c48116-cc6a-11de-b743-054a4f195298>
2011-11-12 19:44:18 +0000

    some refactoring, empty main window

---
+++ MetarWeather/trunk/src/org/sergeys/metar/ui/MetarWeather.java
@@ -0,0 +1,46 @@
+package org.sergeys.metar.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MetarWeather {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					
+					
+					
+					MetarWeather window = new MetarWeather();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MetarWeather() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
