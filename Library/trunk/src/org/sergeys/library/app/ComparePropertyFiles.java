a778b813a0168faae1481198b4e46f27f64fb8ab
sergey.selivanov <sergey.selivanov@41c48116-cc6a-11de-b743-054a4f195298>
2012-03-10 19:20:50 +0000

    NEW - task 14: improve ComparePropertyFiles tool http://code.google.com/p/sergeys-tools/issues/detail?id=14

--- Library/trunk/src/org/sergeys/library/app/ComparePropertyFiles.java
+++ Library/trunk/src/org/sergeys/library/app/ComparePropertyFiles.java
@@ -1,15 +1,21 @@
 package org.sergeys.library.app;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.Set;
 
-public class ComparePropertyFiles {
+import org.sergeys.library.FileUtils;
+
+public abstract class ComparePropertyFiles {
 
     public static void main(String[] args) {
 
@@ -48,47 +54,95 @@ public class ComparePropertyFiles {
                 Properties pAdd = compareProps(propMaster, propTarget);
                 Properties pRemove = compareProps(propTarget, propMaster);
 
-                for(Object key: pAdd.keySet()){
-                    propTarget.put(key, pAdd.get(key));
-                    System.out.println(String.format("Added %s=%s", key, pAdd.get(key)));
-                }
-                for(Object key: pRemove.keySet()){
-                    propTarget.remove(key);
-                    System.out.println(String.format("Removed %s=%s", key, pRemove.get(key)));
-                }
-
+                // read-write as plain text file to save non-unicode text
                 try {
-                    propTarget.store(new FileOutputStream(targetFile), "");
-                } catch (FileNotFoundException e) {
-                    System.out.println(String.format("Cannot save %s: %s", targetFile, e.getMessage()));
-                    e.printStackTrace();
+                    BufferedReader rdr = new BufferedReader(new FileReader(targetFile));
+
+                    File tmp = File.createTempFile(ComparePropertyFiles.class.getName(), "txt");
+                    FileWriter fw = new FileWriter(tmp);
+                    String line = rdr.readLine();
+                    while(line != null){
+
+//System.out.println(String.format("line: %s", line));
+                        boolean match = false;
+                        for(Entry<Object, Object> entry: pRemove.entrySet()) {
+                            if(line.startsWith(entry.getKey().toString() + "=")){
+                                match = true;
+                                System.out.println(String.format("Removed %s=%s", entry.getKey(), entry.getValue()));
+                                break;
+                            }
+                        }
+                        if(!match){
+                            fw.write(line + "\n");
+                        }
+
+                        line = rdr.readLine();
+                    }
+
+                    rdr.close();
+
+                    for(Entry<Object, Object> entry: pAdd.entrySet()) {
+                        fw.write(String.format("%s=%s%n", entry.getKey(), entry.getValue()));
+                        System.out.println(String.format("Added %s=%s", entry.getKey(), entry.getValue()));
+                    }
+
+                    fw.close();
+
+                    FileUtils.copyFile(tmp.getAbsolutePath(), targetFile);
+                    if(!tmp.delete()){
+                        System.out.println(String.format("Cannot delete temp file %s", tmp.getAbsolutePath()));
+                    }
+
+                } catch (FileNotFoundException e1) {
+                    System.out.println(String.format("Cannot save %s: %s", targetFile, e1.getMessage()));
+                    e1.printStackTrace();
                 } catch (IOException e) {
                     System.out.println(String.format("Cannot save %s: %s", targetFile, e.getMessage()));
                     e.printStackTrace();
                 }
+
+
+//                for(Object key: pAdd.keySet()){
+//                    propTarget.put(key, pAdd.get(key));
+//                    System.out.println(String.format("Added %s=%s", key, pAdd.get(key)));
+//                }
+//                for(Object key: pRemove.keySet()){
+//                    propTarget.remove(key);
+//                    System.out.println(String.format("Removed %s=%s", key, pRemove.get(key)));
+//                }
+//
+//                try {
+//                    propTarget.store(new FileOutputStream(targetFile), "");
+//                } catch (FileNotFoundException e) {
+//                    System.out.println(String.format("Cannot save %s: %s", targetFile, e.getMessage()));
+//                    e.printStackTrace();
+//                } catch (IOException e) {
+//                    System.out.println(String.format("Cannot save %s: %s", targetFile, e.getMessage()));
+//                    e.printStackTrace();
+//                }
             }
             else{
                 // just print differences
                 System.out.println(String.format("Properties only in %s:", masterFile));
                 System.out.println();
                 Properties p = compareProps(propMaster, propTarget);
-                for(Object key: p.keySet()){
-                    System.out.println(String.format("%s=%s", key, p.get(key)));
+                for(Entry<Object, Object> entry: p.entrySet()){
+                    System.out.println(String.format("%s=%s", entry.getKey(), entry.getValue()));
                 }
                 System.out.println();
 
                 System.out.println(String.format("Properties only in %s:", targetFile));
                 System.out.println();
                 p = compareProps(propTarget, propMaster);
-                for(Object key: p.keySet()){
-                    System.out.println(String.format("%s=%s", key, p.get(key)));
+                for(Entry<Object, Object> entry: p.entrySet()){
+                    System.out.println(String.format("%s=%s", entry.getKey(), entry.getValue()));
                 }
             }
         }
     }
 
     /**
-     * Returns properties that only present in p1
+     * Returns properties that only present in p1.
      *
      * @param p1
      * @param p2
