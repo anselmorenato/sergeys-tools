c234386dd729763d7d44744a375b974b4f384807
sergey.selivanov <sergey.selivanov@41c48116-cc6a-11de-b743-054a4f195298>
2012-03-10 17:14:20 +0000

    NEW - task 14: improve ComparePropertyFiles tool http://code.google.com/p/sergeys-tools/issues/detail?id=14

--- Library/trunk/src/org/sergeys/library/app/ComparePropertyFiles.java
+++ Library/trunk/src/org/sergeys/library/app/ComparePropertyFiles.java
@@ -1,58 +1,112 @@
 package org.sergeys.library.app;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Properties;
 import java.util.Set;
 
 public class ComparePropertyFiles {
 
-    // TODO: add arguments to automatically add new props and remove unused props from target files
-    // TODO: accept more target files
-
     public static void main(String[] args) {
 
-        if(args.length != 2){
-            System.out.println("Two files are required");
+        if(args.length < 2){
+            System.out.println("Usage: " + ComparePropertyFiles.class.getName() + " [-edit] masterfile targetfile [targetfile2 ...]");
             return;
         }
 
+        boolean bEdit = (args[0].compareTo("-edit") == 0);
 
-        Properties p1 = new Properties();
+        String masterFile = (bEdit) ? args[1] : args[0];
+        ArrayList<String> targetFiles = new ArrayList<String>();
+        for(int i = (bEdit) ? 2 : 1; i < args.length; i++){
+            targetFiles.add(args[i]);
+        }
+
+        Properties propMaster = new Properties();
         try {
-            p1.load(new FileInputStream(args[0]));
+            propMaster.load(new FileInputStream(masterFile));
         } catch (Exception e) {
-            System.out.println(String.format("Cannot open %s: %s", args[0], e.getMessage()));
+            System.out.println(String.format("Cannot open %s: %s", masterFile, e.getMessage()));
             return;
         }
 
-        Properties p2 = new Properties();
-        try {
-            p2.load(new FileInputStream(args[1]));
-        } catch (Exception e) {
-            System.out.println(String.format("Cannot open %s: %s", args[1], e.getMessage()));
-            return;
+        for(String targetFile: targetFiles) {
+            Properties propTarget = new Properties();
+            try {
+                propTarget.load(new FileInputStream(targetFile));
+            } catch (Exception e) {
+                System.out.println(String.format("Cannot open %s: %s", targetFile, e.getMessage()));
+                return;
+            }
+
+            if(bEdit){
+                // modify target
+                Properties pAdd = compareProps(propMaster, propTarget);
+                Properties pRemove = compareProps(propTarget, propMaster);
+
+                for(Object key: pAdd.keySet()){
+                    propTarget.put(key, pAdd.get(key));
+                    System.out.println(String.format("Added %s=%s", key, pAdd.get(key)));
+                }
+                for(Object key: pRemove.keySet()){
+                    propTarget.remove(key);
+                    System.out.println(String.format("Removed %s=%s", key, pRemove.get(key)));
+                }
+
+                try {
+                    propTarget.store(new FileOutputStream(targetFile), "");
+                } catch (FileNotFoundException e) {
+                    System.out.println(String.format("Cannot save %s: %s", targetFile, e.getMessage()));
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    System.out.println(String.format("Cannot save %s: %s", targetFile, e.getMessage()));
+                    e.printStackTrace();
+                }
+            }
+            else{
+                // just print differences
+                System.out.println(String.format("Properties only in %s:", masterFile));
+                System.out.println();
+                Properties p = compareProps(propMaster, propTarget);
+                for(Object key: p.keySet()){
+                    System.out.println(String.format("%s=%s", key, p.get(key)));
+                }
+                System.out.println();
+
+                System.out.println(String.format("Properties only in %s:", targetFile));
+                System.out.println();
+                p = compareProps(propTarget, propMaster);
+                for(Object key: p.keySet()){
+                    System.out.println(String.format("%s=%s", key, p.get(key)));
+                }
+            }
         }
-
-        System.out.println(String.format("Properties only in %s:", args[0]));
-        System.out.println();
-        compareProps(p1, p2);
-        System.out.println();
-
-        System.out.println(String.format("Properties only in %s:", args[1]));
-        System.out.println();
-        compareProps(p2, p1);
     }
 
-    public static void compareProps(Properties p1, Properties p2){
+    /**
+     * Returns properties that only present in p1
+     *
+     * @param p1
+     * @param p2
+     * @return
+     */
+    public static Properties compareProps(Properties p1, Properties p2){
 
-        @SuppressWarnings("unchecked")
-        Set<Object> keys = ((Hashtable<Object, Object>) p1.clone()).keySet();
+        Properties result = new Properties();
+
+        Set<?> keys = ((Hashtable<?, ?>)p1.clone()).keySet();
         keys.removeAll(p2.keySet());
 
         for(Object o: keys){
-            System.out.println(String.format("%s=%s", o, p1.get(o)));
+            //System.out.println(String.format("%s=%s", o, p1.get(o)));
+            result.put(o, p1.get(o));
         }
+
+        return result;
     }
 
 }
