b0301ed16ef699329d91875d46301dfa954d3a54
sergey.selivanov@gmail.com <sergey.selivanov@gmail.com@41c48116-cc6a-11de-b743-054a4f195298>
2012-10-28 15:13:59 +0000

    initial commit

---
+++ PrivoxyTool/trunk/src/org/sergeys/privoxytool/ui/PrivoxyTool.java
@@ -0,0 +1,43 @@
+package org.sergeys.privoxytool.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class PrivoxyTool {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PrivoxyTool window = new PrivoxyTool();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PrivoxyTool() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
