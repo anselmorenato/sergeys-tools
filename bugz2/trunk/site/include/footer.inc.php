9ff09b66deba60360d3f48f8b33b0a7a1e23ff1f
sergey.selivanov@gmail.com <sergey.selivanov@gmail.com@41c48116-cc6a-11de-b743-054a4f195298>
2014-04-05 17:30:46 +0000

    update site

--- bugz2/trunk/site/include/footer.inc.php
+++ bugz2/trunk/site/include/footer.inc.php
@@ -3,26 +3,19 @@
 <!--            Sidebar-->
 <!--        </div>-->
         <div id="footer">
-<!--            <img src="/images/nepipaj.png"></img>-->
             <p>
-<!--            <a href="http://www.spreadbsd.org/aff/557/3">-->
-<!--                <img border="0" alt="SpreadBSD" title="SpreadBSD" src="http://ixsystems.com/images/freebsdbutton1.png"/>-->
-<!--            </a>-->
-
-<!--             <a href="http://www.spreadbsd.org/aff/557/3"> -->
-<!--             	<img border="0" alt="SpreadBSD" title="SpreadBSD" src="/images/SpreadBSD_FreeBSD_Black.png"/> -->
-<!--            	</a> -->
-           	
             <br/>
+            <!--
             Hosted by <a href="http://protechtele.com/whmcs/index.php">PROTECH INC.</a>
+             -->
             </p>
-        </div>    
+        </div>
     </div>
-    
+
 <script type="text/javascript">
   window.onresize = function(){onResize();};
   onResize();
 </script>
-    
+
 </body>
 </html>
