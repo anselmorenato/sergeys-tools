27ddfebf3657ea074809d8a0e73ddd9cae4937f4
sergey.selivanov@gmail.com <sergey.selivanov@gmail.com@41c48116-cc6a-11de-b743-054a4f195298>
2014-04-18 15:24:25 +0000

    gradle build in progress

---
+++ freemind-webstart/trunk/src/main/java/org/sergeys/fmws/FreemindWebstart.java
@@ -0,0 +1,22 @@
+package org.sergeys.fmws;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FreemindWebstart {
+
+    private static Logger log;
+
+    static{
+        log = LoggerFactory.getLogger(FreemindWebstart.class);
+    }
+
+    public static void main(String[] args) {
+        log.info("app started");
+
+        // TODO unpack resources if needed
+        // set home dir
+        // launch main class
+    }
+
+}
