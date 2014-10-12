1f830086d871aaa3771865bf53ea7654c9d8a55e
sergey.selivanov@gmail.com <sergey.selivanov@gmail.com@41c48116-cc6a-11de-b743-054a4f195298>
2014-10-12 17:32:31 +0000

    webstart deployment

--- freemind-webstart/trunk/src/main/java/org/sergeys/fmws/FreemindWebstart.java
+++ freemind-webstart/trunk/src/main/java/org/sergeys/fmws/FreemindWebstart.java
@@ -1,7 +1,15 @@
 package org.sergeys.fmws;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import freemind.main.FreeMindStarter;
 
 public class FreemindWebstart {
 
@@ -12,11 +20,58 @@ public class FreemindWebstart {
     }
 
     public static void main(String[] args) {
+
         log.info("app started");
 
-        // TODO unpack resources if needed
-        // set home dir
-        // launch main class
+        try{
+            // unpack resources if needed
+            String fmHome = System.getProperty("user.home") + File.separator + ".freemind-webstart-home";
+            log.info("home dir " + fmHome);
+            if(!new File(fmHome + File.separator + "patterns.xml").exists()){
+
+                log.info("extracting files");
+
+                InputStream is = FreemindWebstart.class.getResourceAsStream("/freemind-home.jar");
+                JarInputStream jis = new JarInputStream(is);
+                JarEntry je;
+
+                byte[] buffer = new byte[10240];
+
+                while((je = jis.getNextJarEntry()) != null){
+
+                    if(je.isDirectory()){
+                        continue;
+                    }
+
+                    String fileName = je.getName();
+
+                    File newFile = new File(fmHome + File.separator + fileName);
+                    new File(newFile.getParent()).mkdirs();
+
+                    log.info(newFile.getAbsolutePath());
+
+                    FileOutputStream fos = new FileOutputStream(newFile);
+
+                    int len;
+                    while ((len = jis.read(buffer)) > 0) {
+                        fos.write(buffer, 0, len);
+                    }
+
+                    fos.close();
+                }
+
+                jis.close();
+            }
+
+            // set home dir
+            // launch main class
+            //System.setProperty("freemind.base.dir", "d:/junkfreemind");
+            System.setProperty("freemind.base.dir", fmHome);
+            FreeMindStarter.main(args);
+        }
+        catch(Exception ex){
+            log.error("Failed to launch freemind", ex);
+        }
     }
 
 }
