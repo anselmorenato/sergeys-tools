355bbec632d9b34e304258e1ab45b9865bc36355
sergey.selivanov@gmail.com <sergey.selivanov@gmail.com@41c48116-cc6a-11de-b743-054a4f195298>
2014-11-08 15:03:00 +0000

    more icons

--- wcd2/trunk/src/main/webstart/resizepng.cmd
+++ wcd2/trunk/src/main/webstart/resizepng.cmd
@@ -1,1 +1,6 @@
-f:\bin32\imagemagick-6.7.2-10\convert %1 -resize 64x64 converted.png
+set CONVERT=f:\bin32\imagemagick-6.7.2-10\convert 
+
+%CONVERT% %1 -resize 16x16 16.png
+%CONVERT% %1 -resize 32x32 32.png
+%CONVERT% %1 -resize 48x48 48.png
+%CONVERT% %1 -resize 64x64 64.png
